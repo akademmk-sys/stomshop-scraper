3d4a0efb87b3bca86fa3ae1fe325839ca4e37ced
akademmk-sys <286443375+akademmk-sys@users.noreply.github.com>
2026-06-02 22:22:20 +0300

    colly connect

--- main.go
+++ main.go
@@ -13,9 +13,9 @@ func main() {
 	c.OnRequest(func(r *colly.Request) {
 		r.Headers.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36")
 	})
-	c.OnHTML("a", func(e *colly.HTMLElement) {
+	c.OnHTML("#content > p", func(r *colly.HTMLElement) {
 
-		fmt.Println(e.Text)
+		fmt.Println(r.Text)
 	})
 	c.OnError(func(r *colly.Response, err error) {
 		fmt.Println("Ошибка:", r.StatusCode, err)
