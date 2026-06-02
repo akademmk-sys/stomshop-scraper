5be8bdbd0437fc4d6ef388f959536f5bb3c3adbf
akademmk-sys <286443375+akademmk-sys@users.noreply.github.com>
2026-06-02 21:15:15 +0300

    pars_v0.0001

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gocolly/colly/v2"
 )
 
 func main() {
-	c := colly.NewCollector()
+	c := colly.NewCollector(
+		colly.AllowedDomains("stomshop.pro"),
+	)
+	c.OnRequest(func(r *colly.Request) {
+		r.Headers.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36")
+	})
+	c.OnHTML("a", func(e *colly.HTMLElement) {
+
+		fmt.Println(e.Text)
+	})
+	c.OnError(func(r *colly.Response, err error) {
+		fmt.Println("Ошибка:", r.StatusCode, err)
+	})
+	c.Visit("https://stomshop.pro/")
 }
