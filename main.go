8a7085bd81f4c038a6be80b9792a940b1c3fb062
akademmk-sys <286443375+akademmk-sys@users.noreply.github.com>
2026-06-03 23:20:31 +0300

    main_category_parser_added

--- main.go
+++ main.go
@@ -6,6 +6,19 @@ import (
 	"github.com/gocolly/colly/v2"
 )
 
+type ParsedHeaders struct {
+	name string
+	link string
+}
+
+func (p *ParsedHeaders) headerWriter(c, l string) []ParsedHeaders {
+	slice := make([]ParsedHeaders, 0)
+	p.name = c
+	p.link = l
+	slice = append(slice, ParsedHeaders{name: c, link: l})
+	return slice
+}
+
 func main() {
 	c := colly.NewCollector(
 		colly.AllowedDomains("stomshop.pro"),
@@ -13,9 +26,13 @@ func main() {
 	c.OnRequest(func(r *colly.Request) {
 		r.Headers.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36")
 	})
-	c.OnHTML("#content > p", func(r *colly.HTMLElement) {
+	c.OnHTML("#content > div.row.product-layout", func(r *colly.HTMLElement) {
 
-		fmt.Println(r.Text)
+		r.ForEach("div.child h4 a", func(_ int, a *colly.HTMLElement) {
+			var h ParsedHeaders
+			str := h.headerWriter(a.Text, a.Attr("href"))
+			fmt.Println(str)
+		})
 	})
 	c.OnError(func(r *colly.Response, err error) {
 		fmt.Println("Ошибка:", r.StatusCode, err)
