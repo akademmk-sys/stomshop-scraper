4036ca3e15d856a937cf8e92f147c7a7513a55c4
akademmk-sys <286443375+akademmk-sys@users.noreply.github.com>
2026-06-04 20:46:12 +0300

    subcategory parser added

--- main.go
+++ main.go
@@ -6,16 +6,30 @@ import (
 	"github.com/gocolly/colly/v2"
 )
 
-type ParsedHeaders struct {
-	name string
-	link string
+type Category struct {
+	Name string
+	Link string
+	sub  []SubCategory
 }
 
-func (p *ParsedHeaders) headerWriter(c, l string, slice []ParsedHeaders) []ParsedHeaders {
+func (p *Category) HeaderWriter(c, l string, s []SubCategory, slice []Category) []Category {
 
-	p.name = c
-	p.link = l
-	slice = append(slice, ParsedHeaders{name: c, link: l})
+	p.Name = c
+	p.Link = l
+	slice = append(slice, Category{Name: c, Link: l, sub: s})
+	return slice
+}
+
+type SubCategory struct {
+	SubName string
+	SubLink string
+}
+
+func (s *SubCategory) subHeaderWriter(c, l string, slice []SubCategory) []SubCategory {
+
+	s.SubName = c
+	s.SubLink = l
+	slice = append(slice, SubCategory{SubName: c, SubLink: l})
 	return slice
 }
 
@@ -23,20 +37,37 @@ func main() {
 	c := colly.NewCollector(
 		colly.AllowedDomains("stomshop.pro"),
 	)
+
 	c.OnRequest(func(r *colly.Request) {
 		r.Headers.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36")
 	})
-	c.OnHTML("#content > div.row.product-layout", func(r *colly.HTMLElement) {
-		slice := make([]ParsedHeaders, 0)
-		r.ForEach("div.child h4 a", func(_ int, a *colly.HTMLElement) {
-			var h ParsedHeaders
-			slice = h.headerWriter(a.Text, a.Attr("href"), slice)
+
+	slice := make([]Category, 0)
+
+	c.OnHTML("div.mycategory.product-thumb", func(r *colly.HTMLElement) {
+		subSlice := make([]SubCategory, 0)
+		var h Category
+		var d SubCategory
+		r.ForEach("div.child ul.list-unstyled li a", func(_ int, a *colly.HTMLElement) {
+			if a.Text == "показать все ...." {
+				return
+			}
+			subSlice = d.subHeaderWriter(a.Text, a.Attr("href"), subSlice)
 
 		})
-		fmt.Println(slice)
+		r.ForEach("div.child h4 a", func(_ int, a *colly.HTMLElement) {
+
+			slice = h.HeaderWriter(a.Text, a.Attr("href"), subSlice, slice)
+		})
+
 	})
+
 	c.OnError(func(r *colly.Response, err error) {
 		fmt.Println("Ошибка:", r.StatusCode, err)
 	})
+
 	c.Visit("https://stomshop.pro/")
+	fmt.Println(slice[0])
+	fmt.Println(slice[1])
+
 }
