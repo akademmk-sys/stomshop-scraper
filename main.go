1e9b31c1021efb9363b14fb1ed9ead60e6194150
akademmk-sys <286443375+akademmk-sys@users.noreply.github.com>
2026-06-06 09:59:27 +0300

    good working filling slice script

--- main.go
+++ main.go
@@ -15,15 +15,15 @@ type Product struct {
 }
 
 type Category struct {
-	Name string
-	Link string
-	sub  []SubCategory
+	Name   string
+	Link   string
+	SubCat []SubCategory
 }
 
 func (p *Category) HeaderWriter(c, l string, s []SubCategory, slice []Category) []Category {
 
 	p.Name, p.Link = c, l
-	slice = append(slice, Category{Name: c, Link: l, sub: s})
+	slice = append(slice, Category{Name: c, Link: l, SubCat: s})
 	return slice
 }
 
@@ -49,31 +49,30 @@ func main() {
 		r.Headers.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36")
 	})
 
-	slice := make([]Category, 0)
+	AllData := make([]Category, 0)
 
 	c.OnHTML("div.mycategory.product-thumb", func(r *colly.HTMLElement) {
 		var h Category
 		var d SubCategory
-		subSlice := make([]SubCategory, 0)
+		SubHaders := make([]SubCategory, 0)
 
 		r.ForEach("div.child ul.list-unstyled li a", func(_ int, a *colly.HTMLElement) {
 			if a.Text == "показать все ...." {
 				return
 			}
-			subSlice = d.subHeaderWriter(a.Text, a.Attr("href"), subSlice)
-			req, _ := r.Request.New("GET", a.Attr("href"), nil)
-			req.Ctx.Put("sublink", a.Attr("href"))
-			req.Do()
+			SubHaders = d.subHeaderWriter(a.Text, a.Attr("href"), SubHaders)
+
 		})
 
 		r.ForEach("div.child h4 a", func(_ int, a *colly.HTMLElement) {
 
-			slice = h.HeaderWriter(a.Text, a.Attr("href"), subSlice, slice)
+			AllData = h.HeaderWriter(a.Text, a.Attr("href"), SubHaders, AllData)
 		})
 
 	})
 
 	c.OnHTML("div.product-layout div.product-thumb", func(r *colly.HTMLElement) {
+		currentUrl := r.Request.URL.String()
 		unit := Product{
 			PName:   r.ChildText("div.caption a"),
 			PLink:   r.ChildAttr("div.caption a", "href"),
@@ -81,20 +80,15 @@ func main() {
 			Number:  r.ChildText("span.code span"),
 			Country: r.ChildText("div.manufacturer a"),
 		}
-
-		subLink := r.Request.Ctx.Get("sublink")
-
-		if subLink == "" {
-			return
-		}
-
-		for i := range slice {
-			for j := range slice[i].sub {
-				if slice[i].sub[j].SubLink == subLink {
-					slice[i].sub[j].Products = append(slice[i].sub[j].Products, unit)
+		for i := range AllData {
+			for j := range AllData[i].SubCat {
+				if AllData[i].SubCat[j].SubLink == currentUrl {
+					AllData[i].SubCat[j].Products = append(AllData[i].SubCat[j].Products, unit)
 				}
+
 			}
 		}
+
 	})
 
 	c.OnError(func(r *colly.Response, err error) {
@@ -102,10 +96,15 @@ func main() {
 	})
 
 	c.Visit("https://stomshop.pro/")
-	for _, elem := range slice {
+	for i := range AllData {
+		for j := range AllData[i].SubCat {
+			c.Visit(AllData[i].SubCat[j].SubLink)
+		}
+	}
+	for _, elem := range AllData {
 		fmt.Println("============================================================", elem.Name, "============================================================")
 		fmt.Println("_____ ", elem.Link)
-		for _, subElem := range elem.sub {
+		for _, subElem := range elem.SubCat {
 			fmt.Println("+++ ", subElem.SubName, " +++")
 			fmt.Println("____", subElem.SubLink)
 			for _, l := range subElem.Products {
